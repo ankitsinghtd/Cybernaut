55e626f58560c6ebc8d8766a02872a920039a404
Ankit Singh <ankitshekhawat46931@gmail.com>
2023-09-09 02:39:16 +0530

    added report visual elements

--- src/scripts/domain.js
+++ src/scripts/domain.js
@@ -4,6 +4,38 @@ document.getElementById("scan-button").addEventListener('click', async function 
     const baseUrl = document.getElementById("urltoscan").value;
     const analysis = await domainReport(baseUrl);
     displayResult(analysis);
+
+    // Show the hidden tables
+    const dnsTable = document.getElementById("dns-records");
+    const networkTable = document.getElementById("network-statistics");
+    const summary = document.getElementById("summary-box");
+    dnsTable.classList.remove("hidden");
+    networkTable.classList.remove("hidden");
+    summary.classList.remove("hidden");
+
+    // Create and display the pie chart
+    const chartData = {
+        labels: ['Category A', 'Category B', 'Category C'],
+        datasets: [{
+            data: [25, 35, 40], // Adjust the data values as needed
+            backgroundColor: ['#f06', '#f90', '#f33'], // Adjust the colors as needed
+        }]
+    };
+
+    const ctx = document.getElementById("pie-chart").getContext('2d');
+    new Chart(ctx, {
+        type: 'pie',
+        data: chartData,
+        options: {
+            responsive: false,
+            legend: {
+                position: 'bottom',
+                labels: {
+                    fontSize: 16,
+                }
+            },
+        },
+    });
 });
 
 async function domainReport(domain){
