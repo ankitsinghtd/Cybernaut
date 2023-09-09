8b6ad52013ef1a7607ba1c37fa63cbe11bd984eb
Ankit Singh <ankitshekhawat46931@gmail.com>
2023-09-09 20:21:32 +0530

    Updated news page a little

--- src/scripts/news.js
+++ src/scripts/news.js
@@ -1,40 +1,74 @@
-    const apiKey='f0005ff29d41416392b7921594b316fc';
-    const apiUrl='https://newsapi.org/v2/everything?q=';
+const apiKey = '_REDACTED_';
+const apiUrl = 'https://newsapi.org/v2/everything?q=';
+const loadingIndicator = document.getElementById("loading-indicator");
+const cardContainer = document.getElementById("cards-container");
 
-    window.addEventListener('load',()=>fetchnews("cybersecurity"));
+window.addEventListener('load', () => {
+    showLoadingIndicator();
+    fetchNews("cybersecurity");
+});
 
-    async function fetchnews(query){
-        const data=await fetch(`${apiUrl}${query}&apiKey=${apiKey}`);
-        const res=await data.json();
-        bindData(res.articles);
-    }
-    function bindData(articles){
-        const cardContainer=document.getElementById("cards-container");
-        const newsTemplate=document.getElementById("template-news-card");
-        cardContainer.innerHTML="";
-        articles.forEach(articles => {
-            if(!articles.urlToImage) return;
-            const cardClone=newsTemplate.content.cloneNode(true);
-            fillDatainCards(cardClone,articles);
-            cardContainer.appendChild(cardClone);
-        });
-    }
-    async function fillDatainCards(cardClone,articles){
-        const newsImg=cardClone.querySelector("#news-image");
-        const newsTitle=cardClone.querySelector("#news-title");
-        const newsSrc=cardClone.querySelector("#news-source");
-        const newsDesc=cardClone.querySelector("#news-desc");
-        const newsLink=cardClone.querySelector("#news-link");
-        const newsHlink=cardClone.querySelector("#news-h-link");
-        
-        newsImg.src=articles.urlToImage;
-        newsTitle.innerHTML=articles.title;
-        newsDesc.innerHTML=articles.description;
-        newsLink.href=articles.url;
-        newsHlink.href=articles.url;
+async function fetchNews(query) {
+    try {
+        const response = await fetch(`${apiUrl}${query}&apiKey=${apiKey}`);
 
-        const date=new Date(articles.publishedAt).toLocaleString("en-US",{
-            timeZone: "Asia/Jakarta"
-        })
-        newsSrc.innerHTML=`${articles.source.name}, ${date}`;
+        if (!response.ok) {
+            throw new Error(`Network response was not ok (status: ${response.status})`);
+        }
+
+        const data = await response.json();
+        bindData(data.articles);
+    } catch (error) {
+        console.error("Error fetching news:", error);
+        hideLoadingIndicator();
+        displayErrorMessage();
     }
+}
+
+function showLoadingIndicator() {
+    loadingIndicator.style.display = "block";
+    cardContainer.style.display = "none";
+}
+
+function hideLoadingIndicator() {
+    loadingIndicator.style.display = "none";
+}
+
+function displayErrorMessage() {
+    cardContainer.innerHTML = "<p>An error occurred while fetching news. Please try again later.</p>";
+}
+
+function bindData(articles) {
+    hideLoadingIndicator();
+    cardContainer.style.display = "flex";
+    const newsTemplate = document.getElementById("template-news-card");
+    cardContainer.innerHTML = "";
+
+    articles.forEach((article) => {
+        if (!article.urlToImage) return;
+
+        const cardClone = newsTemplate.content.cloneNode(true);
+        fillDatainCards(cardClone, article);
+        cardContainer.appendChild(cardClone);
+    });
+}
+
+async function fillDatainCards(cardClone, articles) {
+    const newsImg = cardClone.querySelector("#news-image");
+    const newsTitle = cardClone.querySelector("#news-title");
+    const newsSrc = cardClone.querySelector("#news-source");
+    const newsDesc = cardClone.querySelector("#news-desc");
+    const newsLink = cardClone.querySelector("#news-link");
+    const newsHlink = cardClone.querySelector("#news-h-link");
+
+    newsImg.src = articles.urlToImage;
+    newsTitle.innerHTML = articles.title;
+    newsDesc.innerHTML = articles.description;
+    newsLink.href = articles.url;
+    newsHlink.href = articles.url;
+
+    const date = new Date(articles.publishedAt).toLocaleString("en-US", {
+        timeZone: "Asia/Jakarta"
+    })
+    newsSrc.innerHTML = `${articles.source.name}, ${date}`;
+}
