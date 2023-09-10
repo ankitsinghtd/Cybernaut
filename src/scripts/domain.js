7da5bc54018d1734c976b32dd0e5f41992c38931
Ankit Singh <ankitshekhawat46931@gmail.com>
2023-09-11 01:34:41 +0530

    error messages in domain scanning

--- src/scripts/domain.js
+++ src/scripts/domain.js
@@ -1,46 +1,67 @@
 let analysis;
 document.getElementById("scan-button").addEventListener('click', async function () {
-    const mainUrl=document.getElementById('urltoscan').value;
-    const response= await fetch('/domain',{
+    const mainUrl = document.getElementById('urltoscan').value;
+    const loadingContainer = document.getElementById('loading-container');
+    const errorMessage = document.getElementById('error-message');
 
-        method:'POST',
-        headers:{
-            'Content-Type':'application/json'
+    errorMessage.style.display = 'none';
+
+    if (!mainUrl) {
+        loadingContainer.style.display = 'none';
+        errorMessage.textContent = 'Please enter a valid URL.';
+        errorMessage.style.display = 'block';
+        return;
+    }
+
+
+    loadingContainer.style.display = 'block';
+
+    const response = await fetch('/domain', {
+
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
         },
-        body:JSON.stringify({Domain:mainUrl})
+        body: JSON.stringify({ Domain: mainUrl })
     });
 
-    if(response.ok){
-        analysis=await response.json();
-
+    if (response.ok) {
+        analysis = await response.json();
         displayResult(analysis);
     }
-    else{
+    else {
+        const errorResponse = await response.json();
+        const errorMessageText = errorResponse.error || 'An unknown error occurred.';
+        errorMessage.textContent = errorMessageText;
+        errorMessage.style.display = 'block';
         console.error("Error finding the domain");
     }
 
     // Show the hidden tables
     const dnsTable = document.getElementById("dns-records");
+    const anchor = document.getElementById('pie-chart');
     const networkTable = document.getElementById("network-statistics");
     const summary = document.getElementById("summary");
     const popularityrank = document.getElementById("popularityrank");
+    loadingContainer.style.display = 'none';
     dnsTable.classList.remove("hidden");
     networkTable.classList.remove("hidden");
+    anchor.scrollIntoView({ behavior: 'smooth' });
     summary.classList.remove("hidden");
     popularityrank.classList.remove("hidden");
 
     // Create and display the pie chart
     console.log(analysis.data.attributes);
-    const Charting=analysis.data.attributes.last_analysis_stats;
-    const malicious=Charting.malicious;
-    const harmless=Charting.harmless;
-    const suspicious=Charting.suspicious;
-    const undetected=Charting.undetected;
+    const Charting = analysis.data.attributes.last_analysis_stats;
+    const malicious = Charting.malicious;
+    const harmless = Charting.harmless;
+    const suspicious = Charting.suspicious;
+    const undetected = Charting.undetected;
     const chartData = {
-        labels: ['malicious','harmless','suspicious','undetected'],
+        labels: ['malicious', 'harmless', 'suspicious', 'undetected'],
         datasets: [{
-            data: [malicious, harmless,suspicious,undetected], // Adjust the data values as needed
-            backgroundColor: ['#f06', '#f90', '#f33','#f104'], // Adjust the colors as needed
+            data: [malicious, harmless, suspicious, undetected], // Adjust the data values as needed
+            backgroundColor: ['#f06', '#f90', '#f33', '#f104'], // Adjust the colors as needed
         }]
     };
 
@@ -59,35 +80,35 @@ document.getElementById("scan-button").addEventListener('click', async function 
         },
     });
     //Summary
-    const Sum=document.getElementById("summary");
-    if((malicious+suspicious+undetected)>harmless){
-        Sum.innerHTML=`The sites is not safe to visit \n malicious:${malicious} || harmless:${harmless} `;
+    const Sum = document.getElementById("summary");
+    if ((malicious + suspicious + undetected) > harmless) {
+        Sum.innerHTML = `The sites is not safe to visit \n malicious:${malicious} || harmless:${harmless} `;
     }
-    else{
-        Sum.innerHTML=`The site is safe to visit.`
+    else {
+        Sum.innerHTML = `The site is safe to visit.`
     }
 });
 
 async function displayResult(result) {
-    const Dns=result.data.attributes.last_analysis_results;
-    const AlienVault=document.getElementById("AV");
-    const SP=document.getElementById("SP");
-    const BV=document.getElementById("BV");  
-    const VX=document.getElementById("VX");
-    const GSB=document.getElementById("GSB");
-    const MAL=document.getElementById("MAL");
-    const SR=document.getElementById("SR");
-    const CS=document.getElementById("CS");
-    const KAS=document.getElementById("Kas");
-    const ET=document.getElementById("ET");
-    AlienVault.innerHTML=Dns.AlienVault.category;
-    SP.innerHTML=Dns.Sophos.category;
-    BV.innerHTML=Dns.Bkav.category;
-    VX.innerHTML=Dns["VX Vault"].category;
-    GSB.innerHTML=Dns["Google Safebrowsing"].category;
-    MAL.innerHTML=Dns.Malwared.category;
-    SR.innerHTML=Dns.SOCRadar.category;
-    CS.innerHTML=Dns.CrowdSec.category;
-    KAS.innerHTML=Dns.Kaspersky.category;
-    ET.innerHTML=Dns.EmergingThreats.category;
+    const Dns = result.data.attributes.last_analysis_results;
+    const AlienVault = document.getElementById("AV");
+    const SP = document.getElementById("SP");
+    const BV = document.getElementById("BV");
+    const VX = document.getElementById("VX");
+    const GSB = document.getElementById("GSB");
+    const MAL = document.getElementById("MAL");
+    const SR = document.getElementById("SR");
+    const CS = document.getElementById("CS");
+    const KAS = document.getElementById("Kas");
+    const ET = document.getElementById("ET");
+    AlienVault.innerHTML = Dns.AlienVault.category;
+    SP.innerHTML = Dns.Sophos.category;
+    BV.innerHTML = Dns.Bkav.category;
+    VX.innerHTML = Dns["VX Vault"].category;
+    GSB.innerHTML = Dns["Google Safebrowsing"].category;
+    MAL.innerHTML = Dns.Malwared.category;
+    SR.innerHTML = Dns.SOCRadar.category;
+    CS.innerHTML = Dns.CrowdSec.category;
+    KAS.innerHTML = Dns.Kaspersky.category;
+    ET.innerHTML = Dns.EmergingThreats.category;
 }
