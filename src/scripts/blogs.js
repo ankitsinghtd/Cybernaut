0016bf4713413c75b5af2f35ca86d8a84c21a845
HelloShiv <swagatswaroopparida@gmail.com>
2023-09-27 18:54:16 +0530

    fixed chatbot icon focus mode

--- src/scripts/blogs.js
+++ src/scripts/blogs.js
@@ -7,8 +7,8 @@ import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "https://w
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  _redacted_
-};
+  
+};  
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
